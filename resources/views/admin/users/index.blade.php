5bce44a2fbbd889b8ee6c02707388a3c1c028142
José Manuel Miranda Villagrán <androidgalaxys42@gmail.com>
2019-10-02 00:59:52 -0500

    Deleting users

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -25,9 +25,14 @@
                                 <th>{{ $user->email }}</th>
                                 <th>{{ implode(', ', $user->roles()->get()->pluck('name')->toArray()) }}</th>
                                 <th>
-                                    <a href="{{ route('admin.users.edit', $user->id) }}">
+                                    <a href="{{ route('admin.users.edit', $user->id) }}" class="float-left">
                                         <button type="button" class="btn btn-primary btn-sm">Edit</button>
                                     </a>
+                                    <form action="{{ route('admin.users.destroy', $user->id) }}" method="post" class="float-left">
+                                        @csrf
+                                        @method('DELETE')
+                                        <button type="submit" class="btn btn-danger btn-sm">Delete</button>
+                                    </form>
                                 </th>
                             </tr>
                         @endforeach
